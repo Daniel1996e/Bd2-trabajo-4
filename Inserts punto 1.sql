f6c4d68363db8974c448fa4ff7553c2147071286
Daniel1996e <43450731+Daniel1996e@users.noreply.github.com>
2019-04-02 20:32:43 -0500

    Inserts

---
+++ Inserts punto 1.sql
@@ -0,0 +1,35 @@
+--Inserts punto 1--
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (1,5000,1,'m');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (2,4900,2,'f');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (3,4950,3,'m');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (4,4800,4,'f');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (5,4850,4,'m');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (6,4900,4,'f');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (7,4950,3,'m');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (8,4870,2,'f');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (9,4950,1,'m');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (10,4600,3,'f');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (11,3000,3,'m');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (12,4500,2,'f');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (13,4750,2,'f');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (14,4450,1,'m');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (15,4000,4,'f');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (16,4950,1,'f');
+INSERT INTO empleado (codigo, salario, depto, sexo) 
+VALUES (17,2000,2,'m');
