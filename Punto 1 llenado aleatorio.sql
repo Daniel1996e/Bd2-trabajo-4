21e8ac11b2ec257d14650d0ffe3381ca5bdbccfe
Daniel1996e <43450731+Daniel1996e@users.noreply.github.com>
2019-04-02 21:53:26 -0500

    Aporte Santiago

---
+++ Punto 1 llenado aleatorio.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE PROCEDURE llenar
+(k IN NUMBER)
+IS
+n NUMBER (8):= k;
+g VARCHAR (1):='';
+d NUMBER (8):=1;
+s NUMBER (8):=1;
+BEGIN
+    delete from empleado;
+
+    FOR i IN 1..n LOOP
+        IF(d=20)THEN
+            d:=1;
+        ELSE
+            d:=d+1;
+        END IF;
+        IF(MOD(i,2)=0)THEN
+            g:='m';
+        ELSE
+            g:='f';
+        END IF;
+        s:=d*750000;
+        
+        INSERT INTO empleado VALUES(i,s,d,g);
+
+    END LOOP;
+    
+END;
+/
